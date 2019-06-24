54ded35adb6d013b046176197c743ddc29f900e1
Chris Daley <chebizarro@gmail.com>
2019-06-23 21:39:21 -0700

    Added ComposerTests

---
+++ tests/ComposerTests.cpp
@@ -0,0 +1,11 @@
+// Rubric
+// Copyright (c) 2019 Chris Daley <chebizarro@gmail.com>
+// This code is licensed under MIT license (see LICENSE.txt for details)
+
+#include <catch2/catch.hpp>
+
+TEST_CASE( "Load Composer Files", "[Composer]" ) {
+
+
+
+}
